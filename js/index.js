b642d1c7027fb154affa8e084e5cfa0d18444710
Cesar Briam Zubilete Sanchez <33007683+bmyab@users.noreply.github.com>
2022-07-03 15:38:21 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,55 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit' ,  e => {
+  e.preventDefault();
+  const  firstname = form['name'].value;
+  const  lastname = form['lastname'].value;
+  const  email = form['email'].value;
+  const  password = form['password'].value;
+
+  if( firstname === ''){
+    addError('name', 'First Name cannot be empty');
+  }else{
+    removeError('name');
+  }
+  if( lastname === ''){
+    addError('lastname' , 'Last Name cannot be empty');
+  }
+  else{
+    removeError('lastname');
+  }
+
+  if ( email === '') {
+		addError('email', 'Email is required');
+	} else if (!isValid(email)) {
+		addError('email', 'Email is not valid');
+	} else {
+		removeError('email');
+	}
+
+  if( password === ''){
+    addError('password' , 'Password cannot be empty');
+  }else{
+    removeError('password');
+  }
+  
+});
+function addError(field , message ){
+  const formControl = form[field].parentNode;
+  formControl.classList.add('error');
+
+  const small = form[field].parentNode.querySelector('small');
+  small.innerText = message;
+  small.style.opacity = '1';
+}
+function removeError(field){
+  const formControl = form[field].parentNode;
+  formControl.classList.remove('error');
+
+  const small = form[field].parentNode.querySelector('small');
+  small.style.opacity = '0';
+}
+function isValid(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
